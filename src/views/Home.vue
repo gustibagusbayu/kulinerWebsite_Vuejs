b644d33a0d218fc16a405a4f51348931822b74f3
Bayu Adi Pramana <gustibagusbayu@gmail.com>
2021-04-15 19:18:06 +0800

    first commit project

---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="home">
+    <Navbar />
+    <div class="container">
+      <Hero />
+
+      <div class="row mt-4">
+        <div class="col">
+          <h2>Best <strong>Foods</strong></h2>
+        </div>
+        <div class="col">
+          <router-link to="/foods" class="btn btn-success float-right"
+            ><b-icon-eye></b-icon-eye>Lihat Semua</router-link
+          >
+        </div>
+      </div>
+
+      <div class="row mb-4">
+        <div class="col-md-4 mt-4" v-for="product in products" :key="product.id">
+          <CardProduct :product="product"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import CardProduct from "@/components/CardProduct.vue";
+import axios from "axios";
+
+export default {
+  name: "Home",
+  components: {
+    Navbar,
+    Hero,
+    CardProduct,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    setProduct(data) {
+      this.products = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/best-products")
+      .then((response) => this.setProduct(response.data))
+      .catch((error) => console.log("Gagal ", error))
+  },
+};
+</script>
